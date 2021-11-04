4fd77647c73560c53aaa0265ed57dcb3b26f8eaf
Xavier MALPARTY <xavier@nimblehq.co>
2021-11-04 18:22:31 +0700

    Initial demo application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { LiffProvider } from 'react-liff';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const liffId = process.env.REACT_APP_LINE_LIFF_ID;
+const stubEnabled = false && process.env.NODE_ENV !== 'production';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LiffProvider liffId={liffId} stubEnabled={stubEnabled}>
+      <App />
+    </LiffProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
